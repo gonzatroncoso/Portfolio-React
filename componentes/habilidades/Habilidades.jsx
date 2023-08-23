67b9fab1ec0477ae3013ab8b4f407bc511401889
gonzalo <gonzalo.troncoso10@hotmail.com>
2023-08-23 18:40:16 -0300

    imagenes linkeadas

--- componentes/habilidades/Habilidades.jsx
+++ componentes/habilidades/Habilidades.jsx
@@ -5,28 +5,28 @@ import './habilidades.css'
 
 const App = () => {
   const images = [
-    {img: "./src/assets/icons8-html-5-70.png",
+    {img: "https://i.ibb.co/s3WVtdb/icons8-git-70.png",
     nombre:"HTML"
     },
-    {img: "./src/assets/icons8-css3-70.png",
+    {img: "https://i.ibb.co/DY4bzRL/icons8-css3-70.png",
     nombre:"CSS"
     },
-    {img: "./src/assets/icons8-javascript-70.png",
+    {img: "https://i.ibb.co/X44H8Lw/icons8-javascript-70.png",
     nombre:"Javascript"
     },
-    {img: "./src/assets/icons8-react-70.png",
+    {img: "https://i.ibb.co/n7phSKs/icons8-react-70.png",
     nombre:"React"
     },
-    {img: "./src/assets/icons8-github-70.png",
+    {img: "https://i.ibb.co/kMMsT1d/icons8-github-70.png",
     nombre:"Github"
     },
-    {img: "./src/assets/icons8-git-70.png",
+    {img: "https://i.ibb.co/s3WVtdb/icons8-git-70.png",
     nombre:"Git"
     },
-    {img: "./src/assets/icons8-sass-70.png",
+    {img: "https://i.ibb.co/DfxF7hS/icons8-sass-70.png",
     nombre:"SASS"
     },
-    {img: "./src/assets/icons8-sql-server-70.png",
+    {img: "https://i.ibb.co/nfhvpJj/icons8-sql-server-70.png",
     nombre:"SQL Server"
     }
 
